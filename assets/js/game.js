eea8271e6e84939383ede68e47dd0ff0f0d459ae
Mark Loercher <markloercher@marks-MacBook-Pro.local>
2021-10-18 10:02:53 -0400

    initial commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!");
+//example of prompt//
+var playerName = window.prompt("What is your robot's name?");
+//variable "saves data inputted by prompt"
+// this creates a function named "fight"
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+//fight();
+
